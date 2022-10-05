03b56fcdedba4cb5b660c54b851611721897edf3
Instructor <tkddls3747@naver.com>
2022-10-05 17:44:37 +0900

    28

--- src/main/java/com/simple/basic/controller/RecodeController.java
+++ src/main/java/com/simple/basic/controller/RecodeController.java
@@ -53,17 +53,17 @@ public class RecodeController {
 	}
 
 	@GetMapping("/recodeList")
-	public String recodeList(@RequestParam("cat_name") String cat_name, Model model) throws Exception {
+	public @ResponseBody String recodeList(@RequestParam("cat_name") String cat_name, Model model) throws Exception {
 
 		List<JoinDTO> nickName = recodeService.nickName();
-		//List<RecodeDTO> list1 = recodeService.recodeList();
-		//List<CategoryDTO> list3 = categoryService.listAll();
+		List<RecodeDTO> list1 = recodeService.recodeList();
+		List<CategoryDTO> list3 = categoryService.listAll();
 		
-		//model.addAttribute("list1", list1);
-		//model.addAttribute("list3", list3);
+		model.addAttribute("list1", list1);
+		model.addAttribute("list3", list3);
 		model.addAttribute("cat_name", cat_name);
 		model.addAttribute("nickName", nickName);
-		return "/recodelist";
+		return "recodelist";
 	}
 	
 	@GetMapping("/recode_List")
